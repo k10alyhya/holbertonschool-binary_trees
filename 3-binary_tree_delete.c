601c0cd8ee7c3dd607436ee21be01e50f56d60c5
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-08-19 14:26:37 +0300

    Delete binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{	
+        return;
+    }
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
